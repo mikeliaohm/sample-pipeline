323d9578c7547d3c570cf6e4f0db5feb6452cdd5
Mike Liao <mike.hm.liao@gmail.com>
2020-12-22 22:14:41 +0800

    Initial Commit

---
+++ test/SamplePipeline.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SamplePipeline.Controllers;
+
+namespace SamplePipeline.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnsMyName() 
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Mike", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
